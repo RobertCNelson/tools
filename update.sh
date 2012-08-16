4cf6afa320d9f6fa28d722c17feee602c9892cc0
Robert Nelson <robertcnelson@gmail.com>
2012-08-16 14:42:19 -0500

    add update script
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+check_root () {
+	if [[ ${UID} -ne 0 ]] ; then
+		echo "$0 must be run as sudo user or root"
+		exit
+	fi
+}
+
+check_git () {
+	unset APT
+	unset PACKAGE
+	if [ ! $(which git) ] ; then
+		echo "Missing git"
+		PACKAGE+="git-core "
+		APT=1
+	fi
+
+	if [ "${APT}" ] ; then
+		echo "Missing Dependicies"
+		echo "Please install: sudo aptitude install ${PACKAGE}"
+		echo "---------------------------------------------------------"
+		exit
+	fi
+}
+
+check_root
+check_git
+
+git pull
+
