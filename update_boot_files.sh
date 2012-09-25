5b6e368f9729c5efe94207706a4244a1334853c7
Robert Nelson <robertcnelson@gmail.com>
2012-09-25 13:06:54 -0500

    mkimage isnt always installed now
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- update_boot_files.sh
+++ update_boot_files.sh
@@ -22,20 +22,22 @@ if [ -f /boot/initrd.img-$(uname -r) ] ; then
 	cp -v /boot/initrd.img-$(uname -r) /boot/uboot/initrd.img
 fi
 
-#legacy uImage support:
-if [ -f /boot/uboot/uImage ] ; then
-	if [ -f /boot/initrd.img-$(uname -r) ] ; then
-		mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n initramfs -d /boot/initrd.img-$(uname -r) /boot/uboot/uInitrd
-	fi
-	if [ -f /boot/uboot/boot.cmd ] ; then
-		mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/boot.cmd /boot/uboot/boot.scr
-		cp -v /boot/uboot/boot.scr /boot/uboot/boot.ini
-	fi
-	if [ -f /boot/uboot/serial.cmd ] ; then
-		mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/serial.cmd /boot/uboot/boot.scr
-	fi
-	if [ -f /boot/uboot/user.cmd ] ; then
-		mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset Nand" -d /boot/uboot/user.cmd /boot/uboot/user.scr
+MKIMAGE=$(which mkimage 2>/dev/null)
+if [ "x${MKIMAGE}" != "x" ] ; then
+	if [ -f /boot/uboot/uImage ] ; then
+		if [ -f /boot/initrd.img-$(uname -r) ] ; then
+			mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n initramfs -d /boot/initrd.img-$(uname -r) /boot/uboot/uInitrd
+		fi
+		if [ -f /boot/uboot/boot.cmd ] ; then
+			mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/boot.cmd /boot/uboot/boot.scr
+			cp -v /boot/uboot/boot.scr /boot/uboot/boot.ini
+		fi
+		if [ -f /boot/uboot/serial.cmd ] ; then
+			mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/serial.cmd /boot/uboot/boot.scr
+		fi
+		if [ -f /boot/uboot/user.cmd ] ; then
+			mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset Nand" -d /boot/uboot/user.cmd /boot/uboot/user.scr
+		fi
 	fi
 fi
 
