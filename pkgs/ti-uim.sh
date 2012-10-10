54790cd1925d517c52cf17ca6668b21a15e7200a
Robert Nelson <robertcnelson@gmail.com>
2012-10-10 15:39:51 -0500

    ti-uim: build and install
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/ti-uim.sh
+++ pkgs/ti-uim.sh
@@ -9,19 +9,14 @@ if [ ! -f ${HOME}/git/ti-uim/.git/config ] ; then
 	git clone git://github.com/RobertCNelson/ti-uim.git ${HOME}/git/ti-uim/
 fi
 
-DPKG_ARCH=$(dpkg --print-architecture | grep arm)
-case "${DPKG_ARCH}" in
-armel)
-	gnu="gnueabi"
-	;;
-armhf)
-	gnu="gnueabihf"
-	;;
-esac
-
 cd ${HOME}/git/ti-uim/
 
 git checkout master -f
 git pull
 git branch ${ti_uim_sha}-build -D || true
 git checkout ${ti_uim_sha} -b ${ti_uim_sha}-build
+
+make
+sudo make install
+make clean
+
