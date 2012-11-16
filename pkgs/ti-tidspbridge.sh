a7436d8d0f05140521e801fea9659f7a03cb2240
Robert Nelson <robertcnelson@gmail.com>
2012-11-16 15:41:07 -0600

    gst-omapfb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/ti-tidspbridge.sh
+++ pkgs/ti-tidspbridge.sh
@@ -43,3 +43,27 @@ git checkout ${git_sha} -b ${git_sha}-build
 make CROSS_COMPILE= 
 sudo make install
 
+git_sha="origin/master"
+project="gst-omapfb"
+server="git://github.com/felipec"
+
+if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
+	git clone ${server}/${project}.git ${HOME}/git/${project}/
+fi
+
+if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
+	rm -rf ${HOME}/git/${project}/ || true
+	echo "error: git failure, try re-runing"
+	exit
+fi
+
+cd ${HOME}/git/${project}/
+
+git checkout master -f
+git pull || true
+git branch ${git_sha}-build -D || true
+git checkout ${git_sha} -b ${git_sha}-build
+
+make CROSS_COMPILE= 
+sudo make install
+
