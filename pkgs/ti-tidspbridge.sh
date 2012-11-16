c087fc0228e483775b7252e7d21675683b184abe
Robert Nelson <robertcnelson@gmail.com>
2012-11-16 15:57:45 -0600

    tidsp rewrite
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/ti-tidspbridge.sh
+++ pkgs/ti-tidspbridge.sh
@@ -7,38 +7,45 @@ network_down () {
 
 ping -c1 www.google.com | grep ttl &> /dev/null || network_down
 
-unset deb_pkgs
-dpkg -l | grep build-essential >/dev/null || deb_pkgs+="build-essential "
-dpkg -l | grep gstreamer-tools >/dev/null || deb_pkgs+="gstreamer-tools "
-dpkg -l | grep libgstreamer0.10-dev >/dev/null || deb_pkgs+="libgstreamer0.10-dev "
+install_pkgs () {
+	unset deb_pkgs
+	dpkg -l | grep build-essential >/dev/null || deb_pkgs+="build-essential "
+	dpkg -l | grep gstreamer-tools >/dev/null || deb_pkgs+="gstreamer-tools "
+	dpkg -l | grep libgstreamer0.10-dev >/dev/null || deb_pkgs+="libgstreamer0.10-dev "
 
-if [ "${deb_pkgs}" ] ; then
-	echo "Installing: ${deb_pkgs}"
-	sudo apt-get update
-	sudo apt-get -y install ${deb_pkgs}
-fi
+	if [ "${deb_pkgs}" ] ; then
+		echo "Installing: ${deb_pkgs}"
+		sudo apt-get update
+		sudo apt-get -y install ${deb_pkgs}
+	fi
+}
+
+git_generic () {
+	echo "Building: ${project}: ${git_sha}"
+	if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
+		git clone ${server}/${project}.git ${HOME}/git/${project}/
+	fi
+
+	if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
+		rm -rf ${HOME}/git/${project}/ || true
+		echo "error: git failure, try re-runing"
+		exit
+	fi
+
+	cd ${HOME}/git/${project}/
+	make clean &>/dev/null
+
+	git checkout master -f
+	git pull || true
+	git branch ${git_sha}-build -D || true
+	git checkout ${git_sha} -b ${git_sha}-build
+}
 
 git_sha="origin/master"
 project="gst-dsp"
 server="git://github.com/felipec"
 
-if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
-	git clone ${server}/${project}.git ${HOME}/git/${project}/
-fi
-
-if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
-	rm -rf ${HOME}/git/${project}/ || true
-	echo "error: git failure, try re-runing"
-	exit
-fi
-
-cd ${HOME}/git/${project}/
-make clean &>/dev/null
-
-git checkout master -f
-git pull || true
-git branch ${git_sha}-build -D || true
-git checkout ${git_sha} -b ${git_sha}-build
+git_generic
 
 ./configure
 make CROSS_COMPILE= 
@@ -48,49 +55,16 @@ git_sha="origin/master"
 project="gst-omapfb"
 server="git://github.com/felipec"
 
-if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
-	git clone ${server}/${project}.git ${HOME}/git/${project}/
-fi
-
-if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
-	rm -rf ${HOME}/git/${project}/ || true
-	echo "error: git failure, try re-runing"
-	exit
-fi
-
-cd ${HOME}/git/${project}/
-make clean &>/dev/null
-
-git checkout master -f
-git pull || true
-git branch ${git_sha}-build -D || true
-git checkout ${git_sha} -b ${git_sha}-build
+git_generic
 
 make CROSS_COMPILE= 
 sudo make install
 
-git_sha="origin/master"
+git_sha="origin/firmware"
 project="dsp-tools"
 server="git://github.com/felipec"
 
-if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
-	git clone ${server}/${project}.git ${HOME}/git/${project}/
-fi
-
-if [ ! -f ${HOME}/git/${project}/.git/config ] ; then
-	rm -rf ${HOME}/git/${project}/ || true
-	echo "error: git failure, try re-runing"
-	exit
-fi
-
-cd ${HOME}/git/${project}/
-make clean &>/dev/null
-
-git checkout master -f
-git pull || true
-
-git branch -D firmware-tmp || true
-git checkout origin/firmware -b firmware-tmp
+git_generic
 
 if [ ! -d /lib/dsp/ ] ; then
 	sudo mkdir -p /lib/dsp || true
@@ -98,13 +72,11 @@ fi
 
 sudo cp -v firmware/test.dll64P /lib/dsp/
 
-git checkout master -f
-git branch -D firmware-tmp || true
+git_sha="origin/master"
+project="dsp-tools"
+server="git://github.com/felipec"
 
-git checkout master -f
-git pull || true
-git branch ${git_sha}-build -D || true
-git checkout ${git_sha} -b ${git_sha}-build
+git_generic
 
 make CROSS_COMPILE= 
 sudo make install
