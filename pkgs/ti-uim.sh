1fb4dee38336ee3fce87b7ce42d914fc1eb78e03
Robert Nelson <robertcnelson@gmail.com>
2012-12-10 15:22:51 -0600

    ti: uim fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/ti-uim.sh
+++ pkgs/ti-uim.sh
@@ -8,13 +8,16 @@ network_down () {
 ping -c1 www.google.com | grep ttl &> /dev/null || network_down
 
 unset deb_pkgs
-dpkg -l | grep build-essential >/dev/null || deb_pkgs+="build-essential "
+dpkg -l | grep build-essential >/dev/null || deb_pkgs+=" build-essential"
+dpkg -l | grep bluetooth >/dev/null || deb_pkgs+=" bluetooth"
 
-echo "Installing: ${deb_pkgs}bluetooth"
-sudo apt-get update
-sudo apt-get -y install ${deb_pkgs}bluetooth
+if [ "${deb_pkgs}" ] ; then
+	echo "Installing: ${deb_pkgs}"
+	sudo apt-get update
+	sudo apt-get -y install ${deb_pkgs}
+fi
 
-it_sha="origin/master"
+git_sha="origin/master"
 project="ti-uim"
 server="git://github.com/RobertCNelson"
 system=$(lsb_release -sd | awk '{print $1}')
