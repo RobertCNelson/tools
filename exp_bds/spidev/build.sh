29bfbcfa9567e5671986d89af838b3b096583b32
Robert Nelson <robertcnelson@gmail.com>
2013-03-29 16:36:28 -0500

    spidev: works fine
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- exp_bds/spidev/build.sh
+++ exp_bds/spidev/build.sh
@@ -16,6 +16,23 @@ if [ "${deb_pkgs}" ] ; then
 	sudo apt-get -y install ${deb_pkgs}
 fi
 
-gcc spidev_test.c -o spitest
+sudo gcc spidev_test.c -o spitest
+
+echo ""
+echo "spidev_test.c should output"
+echo ""
+echo "spi mode: 0"
+echo "bits per word: 8"
+echo "max speed: 500000 Hz (500 KHz)"
+echo ""
+echo "FF FF FF FF FF FF"
+echo "40 00 00 00 00 95"
+echo "FF FF FF FF FF FF"
+echo "FF FF FF FF FF FF"
+echo "FF FF FF FF FF FF"
+echo "DE AD BE EF BA AD"
+echo "F0 0D "
+echo ""
+
 echo "Beagle"
 echo "sudo ./spitest -D /dev/spidev3.0"
