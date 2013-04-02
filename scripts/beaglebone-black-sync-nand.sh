918aa604e7e44e0fba71d73559c590e8dfd64632
Robert Nelson <robertcnelson@gmail.com>
2013-04-02 14:51:13 -0500

    first pass
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/beaglebone-black-sync-nand.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2013 Robert Nelson <robertcnelson@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+network_down () {
+	echo "Network Down"
+	exit
+}
+
+ping -c1 www.google.com | grep ttl &> /dev/null || network_down
+
+unset deb_pkgs
+dpkg -l | grep build-essential >/dev/null || deb_pkgs+="parted "
+
+if [ "${deb_pkgs}" ] ; then
+	echo "Installing: ${deb_pkgs}"
+	sudo apt-get update
+	sudo apt-get -y install ${deb_pkgs}
+fi
