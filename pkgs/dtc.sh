9ff517e709235b1f39833cd74ac4f4781971114b
Robert Nelson <robertcnelson@gmail.com>
2013-05-31 19:17:27 -0500

    dtc: dash compaitable
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/dtc.sh
+++ pkgs/dtc.sh
@@ -1,14 +1,14 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 network_down () {
 	echo "Network Down"
 	exit
 }
 
-ping -c1 www.google.com | grep ttl &> /dev/null || network_down
+ping -c1 www.google.com | grep ttl > /dev/null 2>&1 || network_down
 
 unset deb_pkgs
-dpkg -l | grep build-essential >/dev/null || deb_pkgs+="build-essential "
+dpkg -l | grep build-essential >/dev/null || deb_pkgs="build-essential "
 
 if [ "${deb_pkgs}" ] ; then
 	echo "Installing: ${deb_pkgs}"
