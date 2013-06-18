881f2e8defccd2c224aacf240faba8520d0a6992
Robert Nelson <robertcnelson@gmail.com>
2013-06-18 16:40:03 -0500

    debian: try this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian/small-lxde-desktop.sh
+++ debian/small-lxde-desktop.sh
@@ -2,6 +2,13 @@
 
 sudo apt-get update
 sudo apt-get -y upgrade
+
 #sudo apt-get -y install task-lxde-desktop
+#Wheezy: 709 New; 449MB; 1276MB
+
 #http://packages.debian.org/wheezy/task-lxde-desktop
-sudo apt-get -y install lightdm lxde task-desktop
+#sudo apt-get -y install lightdm lxde task-desktop
+#Wheezy: 1189 New, 756MB; 2135MB
+
+sudo apt-get install lightdm lxde task-desktop
+
