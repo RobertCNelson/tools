a9da899af6216217ee2ec524c13b0b8ba050decf
Robert Nelson <robertcnelson@gmail.com>
2013-06-18 16:50:37 -0500

    lxde: minimal
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian/small-lxde-desktop.sh
+++ debian/small-lxde-desktop.sh
@@ -3,12 +3,16 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
-#sudo apt-get -y install task-lxde-desktop
-#Wheezy: 709 New; 449MB; 1276MB
-
 #http://packages.debian.org/wheezy/task-lxde-desktop
+
+#Wheezy: 709 pkgs, 449MB, 1276MB
+#sudo apt-get -y install task-lxde-desktop
+
+#Wheezy: 1189 pkgs, 756MB, 2135MB
 #sudo apt-get -y install lightdm lxde task-desktop
-#Wheezy: 1189 New, 756MB; 2135MB
 
-sudo apt-get install lightdm lxde task-desktop
+#Wheezy: 402 pkgs, 159MB, 444MB
+#sudo apt-get -y install lightdm lxde
 
+#Wheezy: 281 pkgs, 94.9MB, 280MB
+sudo apt-get -y install lightdm lxde-core
