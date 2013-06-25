e2172d3ce69747ed8d665adb5e38f1599bbe7bba
Robert Nelson <robertcnelson@gmail.com>
2013-06-25 08:30:41 -0500

    lxde: set cursor to black
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu/small-lxde-desktop.sh
+++ ubuntu/small-lxde-desktop.sh
@@ -7,7 +7,11 @@ sudo apt-get -y upgrade
 #sudo apt-get -y install lightdm lxde-core
 
 #Ubuntu Raring:
-sudo apt-get -y install lxde-core slim xserver-xorg-video-modesetting xserver-xorg
+sudo apt-get -y install lxde-core slim xserver-xorg-video-modesetting xserver-xorg dmz-cursor-theme
+
+sudo update-alternatives --config x-cursor-theme <<-__EOF__
+1
+__EOF__
 
 if [ "x${USER}" != "xroot" ] ; then
 	echo "#!/bin/sh" > ${HOME}/.xinitrc
