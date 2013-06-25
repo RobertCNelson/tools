6a958a683e34d30e23f60cee1a302335073838d4
Robert Nelson <robertcnelson@gmail.com>
2013-06-25 11:23:12 -0500

    debian: lxde: add xrandr
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian/small-lxde-desktop.sh
+++ debian/small-lxde-desktop.sh
@@ -3,24 +3,11 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
-#http://packages.debian.org/wheezy/task-lxde-desktop
+sudo apt-get -y install lightdm lxde-core x11-xserver-utils
 
-#Wheezy: 709 pkgs, 449MB, 1276MB
-#sudo apt-get -y install task-lxde-desktop
-
-#Wheezy: 1189 pkgs, 756MB, 2135MB
-#sudo apt-get -y install lightdm lxde task-desktop
-
-#Wheezy: 402 pkgs, 159MB, 444MB
-#sudo apt-get -y install lightdm lxde
-
-#Wheezy: 281 pkgs, 94.9MB, 280MB
-sudo apt-get -y install lightdm lxde-core
-
-#We know the user/password...
-if [ -f /etc/rcn-ee.conf ] ; then
-	username=$(echo ${USER})
-	if [ "x${username}" != "xroot" ] ; then
-		sudo /usr/lib/arm-linux-gnueabihf/lightdm/lightdm-set-defaults --autologin ${username}
-	fi
+if [ "x${USER}" != "xroot" ] ; then
+	sudo /usr/lib/arm-linux-gnueabihf/lightdm/lightdm-set-defaults --autologin ${USER}
+else
+	echo "To enable autologin:"
+	echo "sudo /usr/lib/arm-linux-gnueabihf/lightdm/lightdm-set-defaults --autologin username"
 fi
