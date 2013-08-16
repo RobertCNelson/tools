e4fe5d7a5bcc5e5c22fac63bce46d8670438b501
Robert Nelson <robertcnelson@gmail.com>
2013-08-16 16:22:47 -0500

    omapdrm: add more of xorg.conf
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/ti-omapdrm.sh
+++ pkgs/ti-omapdrm.sh
@@ -129,11 +129,26 @@ if [ -f /etc/X11/xorg.conf ] ; then
 fi
 
 cat > /tmp/xorg.conf <<-__EOF__
+	Section "Monitor"
+	        Identifier      "Builtin Default Monitor"
+	EndSection
+
 	Section "Device"
-	        Identifier      "omap"
+	        Identifier      "Builtin Default fbdev Device 0"
 	        Driver          "omap"
 	EndSection
 
+	Section "Screen"
+	        Identifier      "Builtin Default fbdev Screen 0"
+	        Device          "Builtin Default fbdev Device 0"
+	        Monitor         "Builtin Default Monitor"
+	        DefaultDepth    16
+	EndSection
+
+	Section "ServerLayout"
+	        Identifier      "Builtin Default Layout"
+	        Screen          "Builtin Default fbdev Screen 0"
+	EndSection
 __EOF__
 
 sudo cp -v /tmp/xorg.conf /etc/X11/xorg.conf
