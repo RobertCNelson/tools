ff0c7f39a8b00e0b281cc8c0460318c38091fe1a
Robert Nelson <robertcnelson@gmail.com>
2013-11-21 13:13:34 -0600

    and this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pkgs/cloud9.sh
+++ pkgs/cloud9.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install nodejs
+release=`lsb_release -sc`
+
+if [ "x$release" = "xwheezy" ] ; then
+	sudo sh -c "echo 'deb http://ftp.debian.org/debian wheezy-backports main contrib non-free' >> /etc/apt/sources.list"
+	sudo apt-get update
+	sudo apt-get -y -t wheezy-backports install nodejs
+else
+	sudo apt-get -y install nodejs
+fi
 
 if [ ! -f /usr/bin/node ] ; then
 	sudo ln -s /usr/bin/nodejs /usr/bin/node
